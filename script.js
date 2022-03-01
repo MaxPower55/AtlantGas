9c6f3d60638a36891bff133f6a3068b2eaeb2988
MaxPower55 <andrewrubanov@gmail.com>
2022-03-01 17:33:49 +0300

    popup complete

---
+++ script.js
@@ -0,0 +1,53 @@
+var carSize = document.querySelectorAll('.block3__option');
+carSize.forEach(button => {
+    button.addEventListener('click', changeSize)
+});
+
+function changeSize(event) {
+    var carArray = [{
+        name: 'Малый газовоз на базе автомобиля УАЗ',
+        image: 'img/carSize-little.png'
+        }, {
+        name: 'Малый газовоз на базе автомобиля Iveco',
+        image: 'img/carSize-small.png'
+        }, {
+        name: 'Средний газовоз на базе автомобиля Камаз',
+        image: 'img/carSize-medium.png'
+        }, {
+        name: 'Большая цистерна-прицеп',
+        image: 'img/carSize-big.png'
+        }
+    ];
+    var a = parseInt(event.target.dataset.num);
+    var carImage = document.getElementById('carImage');
+    carImage.setAttribute('src', carArray[a].image);
+    for(i = 0; i < carSize.length; i++) {
+        carSize[i].className = 'block3__option';
+    }
+    carSize[a].className = 'block3__option block3__option-accent';
+    carImage.setAttribute('alt',carArray[a].name);
+}
+
+
+var popup__close = document.getElementById('popup__close');
+popup__close.addEventListener('click', hidePopup, false);
+
+var btnsZakaz = document.getElementsByClassName('btnZakaz');
+for(i = 0; i < btnsZakaz.length; i++) {
+    btnsZakaz[i].addEventListener('click', showPopup, false);
+};
+
+function showPopup() {
+    var wrap = document.getElementById('wrapper');
+    var popup = document.getElementById('popup');
+    wrap.style.opacity = '0.1';
+    wrap.style.userSelect = 'none';
+    popup.style.display = 'block';
+}
+function hidePopup() {
+    var wrap = document.getElementById('wrapper');
+    var popup = document.getElementById('popup');
+    wrap.style.opacity = '1';
+    wrap.style.userSelect = 'auto';
+    popup.style.display = 'none';
+}
